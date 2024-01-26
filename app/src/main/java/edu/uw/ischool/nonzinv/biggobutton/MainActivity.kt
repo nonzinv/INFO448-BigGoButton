b67157a0e7b3f41aeec7a257127aad10f4ec01e5
nonzinv <nonzinv@uw.edu>
2024-01-26 14:26:38 -0800

    some changes

---
+++ app/src/main/java/edu/uw/ischool/nonzinv/biggobutton/MainActivity.kt
@@ -0,0 +1,40 @@
+package edu.uw.ischool.nonzinv.biggobutton
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlin.random.Random
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private var counter = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        button.setOnClickListener {
+            counter++
+            countButton()
+        }
+    }
+
+    private fun countButton() {
+        var buttontext = ""
+        buttontext = if(counter < 2) {
+            "You have pushed me 1 time!"
+        } else {
+            "You have pushed me $counter times!"
+        }
+        button.text = buttontext
+        val random = Random
+        val bcolor = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
+        val tcolor = Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
+        button.setBackgroundColor(bcolor)
+        button.setTextColor(tcolor)
+    }
+
+
+}
